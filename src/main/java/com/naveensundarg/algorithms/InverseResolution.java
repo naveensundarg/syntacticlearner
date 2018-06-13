72d9ec8a21ec89d27847896a29962a8ac21fa3a6
Naveen Sundar Govindarajulu <naveensundarg@gmail.com>
2018-06-13 07:13:30 -0700

    Example and other changes.

--- src/main/java/com/naveensundarg/algorithms/InverseResolution.java
+++ src/main/java/com/naveensundarg/algorithms/InverseResolution.java
@@ -2,6 +2,7 @@ package com.naveensundarg.algorithms;
 
 import com.naveensundarg.shadow.prover.representations.cnf.Clause;
 import com.naveensundarg.shadow.prover.representations.formula.Formula;
+import org.apache.commons.lang3.NotImplementedException;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.Set;
@@ -11,7 +12,7 @@ public class InverseResolution {
 
     public static Pair<Clause, Clause> inverseResolve(Clause output, Set<Formula> background){
 
-        return
+        throw new NotImplementedException("not yet implemented :)");
     }
 
 }
---
+++ src/main/java/com/naveensundarg/experiments/Traits.java
@@ -0,0 +1,37 @@
+package com.naveensundarg.experiments;
+
+import com.naveensundarg.algorithms.FirstOrderAntiUnifier;
+import com.naveensundarg.shadow.prover.representations.value.Value;
+import com.naveensundarg.shadow.prover.utils.Reader;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Traits {
+
+    public static void main(String[] args) {
+
+
+
+
+            List<Value> inputs = inputs("(trait (Believes! a1 (holds (old x) t1)) (happens (utters a1 (old x)) (+ t1 1)))",
+                                        "(trait (Believes! a2 (holds (new y) t2)) (happens (utters a2 (new y)) (+ t2 1)))",
+                                        "(trait (Believes! a3 (holds (new y) t3)) (happens (utters a3 (new y)) (+ t3 1)))");
+
+            System.out.println(FirstOrderAntiUnifier.antiUnify(inputs));
+
+    }
+
+    private static List<Value> inputs(String... inputs){
+
+        return Arrays.stream(inputs).map(x-> {
+            try {
+                return Reader.readLogicValueFromString(x);
+            } catch (Reader.ParsingException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }).collect(Collectors.toList());
+    }
+}
